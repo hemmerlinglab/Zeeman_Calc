530bd1d7a53fe67b4fa64c6114f8253835019d41
Hemmerling <hemmerlinglab@gmail.com>
2019-07-10 17:56:10 -0700

    removing unnessary file

--- yb_calc_zeeman.py
+++ yb_calc_zeeman.py
@@ -13,7 +13,7 @@ import pylab as plb
 v0     = 150   # m/s
 lbda   = 398.9 # nm
 freq   = 391 # THz
-muB    = 1 
+muB    = 9.274009994*10**(-24) # bohr magneton in SI units
 c      = 299792458 
 h      = 6.62607004*10**(-34) # Energy in Joules * s
 M      = 173*1.66054*10**(-27)
@@ -23,7 +23,7 @@ hbar   = h*2*np.pi
 a_max  = hbar*k*gamma/(2*M)
 k      = 2*np.pi/lbda
 
-B0 = h*v0*freq/(c*muB) #initial B field
+B0 = h*v0/(lbda*muB) #initial B field
 L0 = v0**(2)/a_max     # stopping distance
 
 start_z = 0
