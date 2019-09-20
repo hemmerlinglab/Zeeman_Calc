4302743918b91d5c182ef197d48f2954b20888a3
jdaniel4103 <49174544+jdaniel4103@users.noreply.github.com>
2019-09-20 16:46:38 -0700

    updated to work for a maximum of 4 magnets

--- zeeman_slower_v2.py
+++ zeeman_slower_v2.py
@@ -5,10 +5,10 @@ from scipy.optimize import fsolve
 M = 4*6335 # uo*M/2
 R = .25*25.4e-3 # m
 L = .75*25.4e-3 # m
-nmags = 10
+nmags = 13
 B0 = 268 # Gs
 L0 = .3 # m
-switch_point = 5 # from stacked to unstacked
+switch_point = 6 # from stacked to unstacked
 ys = np.linspace(0,L0,nmags+1)[:-1] # m
 
 sols = []
@@ -25,16 +25,20 @@ for i in range(ni):
 	sols.append(roots[0]-L)
 	n = (100*(i+1)//ni)
 	print('[{}{}] {}'.format('|'*n,' '*(100-n),100*(i+1)/ni),end='\r')
-print(' '*120)
+print(' '*125)
 rmin = 2.5*2.54/2
-print('Minimum spacing: {} cm'.format(2*R*100))
-print('Magnet spacing: {} cm'.format(L0*100/(nmags+1)))
+smin = 2*R*100
+spacing = L0*100/(nmags+1)
+print('Total Number of Magnets: {}'.format(4*switch_point+2*(nmags-switch_point)))
+print('Transition from 2 -> 1 magnets per side: {} -> {}'.format(switch_point,switch_point+1))
+print('Minimum spacing: {} cm'.format(smin))
+print('Magnet spacing: {} cm'.format(spacing))
 print('Minimum radius: {} cm'.format(rmin))
 print('Total Slower Length: {} cm'.format((nmags*L0/(nmags+1)+2*R)*100))
 for j in range(len(sols)):
 	if j == 0:
-		print('- 2 Magnets on Each Side')
+		print('\n- 2 Magnets on Each Side')
 	elif j == switch_point:
 		print('- 1 Magnet on Each Side')
 	print('Magnet {}: {} cm'.format(j+1,sols[j]*100))
-print('Simulation Successful: {}'.format(np.min(sols)*100>rmin))
+print('\nSimulation Successful: {}'.format(np.min(sols)*100>rmin and spacing>smin))
