5f4a01c8ba8720ab6f97c937ad26bca3a0aeebc0
jdaniel4103 <49174544+jdaniel4103@users.noreply.github.com>
2019-10-10 16:36:05 -0700

    possible configuration

--- bfieldtest_v2.py
+++ bfieldtest_v2.py
@@ -9,17 +9,19 @@ L = .75*25.4 # length (mm)
 xo = .25*25.4 # x offset (mm)
 yo = 1*25.4 # y offset (mm)
 zo = (2.5/2)*25.4
-d1 = .003
-d2 = .077
-d3 = .165
-d4 = .276
-d5 = .3
-d6 = .4
-d7 = .421
-d8 = .6
-d9 = .948
-d10 = .948
-d11 = 1.2
+d1 = .19
+d2 = .35
+d3 = .38
+d4 = .44
+d5 = .53
+d6 = .7
+
+d7 = 1.15
+d8 = 1.5
+d9 = 1.55
+
+d10 = 1.25
+d11 = 1.65
 
 print('Initializing sources...')
 s11 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,0,-d1*25.4-zo])
@@ -48,14 +50,20 @@ s53 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,4*yo,+d5*25.4+zo
 s61 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,5*yo,-d6*25.4-zo])
 s63 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,5*yo,+d6*25.4+zo])
 
-s71 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,6*yo,-d7*25.4-zo])
-s73 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,6*yo,+d7*25.4+zo])
+s71 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,6*yo,-d7*25.4-zo])
+s72 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,6*yo,-d7*25.4-zo])
+s73 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,6*yo,+d7*25.4+zo])
+s74 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,6*yo,+d7*25.4+zo])
 
-s81 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,7*yo,-d8*25.4-zo])
-s83 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,7*yo,+d8*25.4+zo])
+s81 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,7*yo,-d8*25.4-zo])
+s82 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,7*yo,-d8*25.4-zo])
+s83 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,7*yo,+d8*25.4+zo])
+s84 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,7*yo,+d8*25.4+zo])
 
-s91 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,8*yo,-d9*25.4-zo])
-s93 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,8*yo,+d9*25.4+zo])
+s91 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,8*yo,-d9*25.4-zo])
+s92 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,8*yo,-d9*25.4-zo])
+s93 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[-xo,8*yo,+d9*25.4+zo])
+s94 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[xo,8*yo,+d9*25.4+zo])
 
 s101 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,9*yo,-d10*25.4-zo])
 s103 = magpy.source.magnet.Cylinder(mag=[0,0,M],dim=[D,L],pos=[0,9*yo,+d10*25.4+zo])
@@ -96,11 +104,17 @@ cz = magpy.Collection(s11,
 					  s61,
 					  s63,
 					  s71,
+					  s72,
 					  s73,
+					  s74,
 					  s81,
+					  s82,
 					  s83,
+					  s84,
 					  s91,
+					  s92,
 					  s93,
+					  s94,
 					  s101,
 					  s103,
 					  s111,
@@ -132,6 +146,7 @@ plt.plot(ys/10,calc,color='r',linestyle='--')
 
 
 h = .2
+flng = 6 # in
 plt.axvline(0,ymin=0,ymax=h,color='g')
 plt.axvline(.1*yo,ymin=0,ymax=h,color='g')
 plt.axvline(.2*yo,ymin=0,ymax=h,color='g')
@@ -143,21 +158,22 @@ plt.axvline(.7*yo,ymin=0,ymax=h,color='g')
 plt.axvline(.8*yo,ymin=0,ymax=h,color='g')
 plt.axvline(.9*yo,ymin=0,ymax=h,color='g')
 plt.axvline(yo,ymin=0,ymax=h,color='g')
+plt.axvline(flng*2.54,ymin=0,ymax=.1,color='y')
+plt.axvline((flng+2)*2.54,ymin=0,ymax=.1,color='y')
 
 plt.title('Magnetic Field Intensity Along Beamline')
 plt.xlabel('Beamline Location (cm)')
 plt.ylabel('Magnetic Field (Gs)')
 
 
-# plt.figure(figsize=(8,8))
-# zs = np.linspace(-5*25.4,5*25.4,100)
-# Btot = np.array([[cz.getB([0,y,z]) for y in ys] for z in zs])
-# Ym,Zm = np.meshgrid(ys,zs)
-# U,V = Btot[:,:,1], Btot[:,:,2]
-# plt.pcolor(ys,zs,np.log(U**2+V**2))
-# plt.streamplot(Ym,Zm,U,V,color='w',density=2)
-# plt.margins(0,0)
-
+plt.figure(figsize=(8,8))
+zs = np.linspace(-5*25.4,5*25.4,100)
+Btot = np.array([[cz.getB([0,y,z]) for y in ys] for z in zs])
+Ym,Zm = np.meshgrid(ys,zs)
+U,V = Btot[:,:,1], Btot[:,:,2]
+plt.pcolor(ys,zs,np.log(U**2+V**2))
+plt.streamplot(Ym,Zm,U,V,color='w',density=2)
+plt.margins(0,0)
 
 cz.displaySystem()
 
